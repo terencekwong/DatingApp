dca20ff6c564bf9d7fd882a912ff93c60d20e509
Terence <terence.kwong@health.nsw.gov.au>
2019-10-29 17:26:33 +1100

    Section 11 -- 106  --

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -39,6 +39,7 @@ namespace DatingApp.API
             services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);;
             
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
