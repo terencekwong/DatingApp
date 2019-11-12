79487d773b6ed97fdfc74d67edcfd0fc785f5a89
Terence <terence.kwong@health.nsw.gov.au>
2019-11-12 16:53:37 +1100

    Section 16 End

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { Message } from 'src/app/_modules/message';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -23,8 +24,19 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    return this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId).subscribe(messages => {
-      this.messages = messages;
+    const currentUserId = +this.authService.decodedToken.nameid;
+    return this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
+      .subscribe(messages => {
+        this.messages = messages;
     }, error => {
       this.alertify.error(error);
     });
